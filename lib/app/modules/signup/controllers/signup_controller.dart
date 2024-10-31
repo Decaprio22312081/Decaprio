0033aab34a078448d9281befb2d27904145afe98
decodecaprio <decodecaprio@gmail.com>
2024-10-31 00:54:43 +0000

    first commit

---
+++ lib/app/modules/signup/controllers/signup_controller.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+class SignupController extends GetxController {
+  //TODO: Implement SignupController
+
+  final count = 0.obs;
+
+  get cEmail => null;
+
+  get cPass => null;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+}
